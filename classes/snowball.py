b21bd4962c1be81c9e31990e5250ba9ed0d1bc08
MajinClraik <64064308+MajinClraik@users.noreply.github.com>
2020-05-19 14:58:28 +1000

    Added settings & np check

--- classes/snowball.py
+++ classes/snowball.py
@@ -7,15 +7,27 @@ class SnowballAB:
         self.neo = neo
         self.sdb = SDB(self.neo)
         self.functions = Functions()
+        self.getSettings()
+        self.BuyDelay = 1800
+
+    def getSettings(self):
+        self.BuyDelay = int(self.functions.getSettings('BuyDelay').split(':')[1].strip())
 
     def SnowballAB(self, username):
         self.functions.createTaskData('SnowballAB', username)
-        if time.time() - float(self.functions.lastRun('SnowballAB', username)) >= 1800:
-            resp = self.neo.post('faerieland/springs.phtml', {'type': 'purchase'}, 'http://www.neopets.com/faerieland/springs.phtml')
-            if self.functions.contains(resp.text, 'buy one item every 30 minutes'):
-                self.functions.log('Snowball AB: Can\'t Buy A Snowball Yet!')
+        if self.BuyDelay < 1800:
+            self.BuyDelay = 1800
+        if time.time() - float(self.functions.lastRun('SnowballAB', username)) >= self.BuyDelay:
+            resp = self.neo.get('faerieland/springs.phtml')
+            currentNp = self.functions.getNp(resp.text)
+            if currentNp >= 25:
+                resp = self.neo.post('faerieland/springs.phtml', {'type': 'purchase'}, 'http://www.neopets.com/faerieland/springs.phtml')
+                if self.functions.contains(resp.text, 'buy one item every 30 minutes'):
+                    self.functions.log('Snowball AB: Can\'t Buy A Snowball Yet!')
+                else:
+                    self.neo.get('faerieland/process_springs.phtml?obj_info_id=8429', 'http://www.neopets.com/faerieland/springs.phtml')
+                    self.functions.log('Snowball AB: Purchased x1 Sticky Snowball!')
+                    self.sdb.deposit()
             else:
-                self.neo.get('faerieland/process_springs.phtml?obj_info_id=8429', 'http://www.neopets.com/faerieland/springs.phtml')
-                self.functions.log('Snowball AB: Purchased x1 Sticky Snowball!')
-                self.sdb.deposit()
+                self.functions.log('Snowball AB: You don\'t have enough neopoints to buy a snowball!')
             self.functions.updateLastRun('SnowballAB', username)
