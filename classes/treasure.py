fc7240c5adec9fd9f9f51d18543343319db2d40b
MajinClraik <64064308+MajinClraik@users.noreply.github.com>
2020-05-22 16:11:03 +1000

    Added account too young check

--- classes/treasure.py
+++ classes/treasure.py
@@ -9,11 +9,14 @@ class BuriedTreasure:
 
     def BuriedTreasure(self, username):
         self.functions.createTaskData('BuriedTreasure', username)
-        if time.time() - float(self.functions.lastRun('BuriedTreasure', username)) >= 10800:
+        if time.time() - float(self.functions.lastRun('BuriedTreasure', username)) >= 86400:
             resp = self.neo.get('pirates/buriedtreasure/buriedtreasure.phtml?', 'https://thedailyneopets.com/dailies')
-            if not self.functions.contains(resp.text, 'you have to wait another'):
-                x, y = random.randint(25, 450), random.randint(45, 460)
-                resp = self.neo.get('pirates/buriedtreasure/buriedtreasure.phtml?%s,%s' % (x, y), 'http://www.neopets.com/pirates/buriedtreasure/buriedtreasure.phtml?')
-                gamePrize = self.functions.getBetween(resp.text, '<b><center>', '</center></b>')
-                self.functions.log('Buried Treasure: %s' % gamePrize)
-                self.functions.updateLastRun('BuriedTreasure', username)
+            if not self.functions.contains(resp.text, 'Your account must be at least <b>24</b> hours old to play'):
+                if not self.functions.contains(resp.text, 'you have to wait another'):
+                    x, y = random.randint(25, 450), random.randint(45, 460)
+                    resp = self.neo.get('pirates/buriedtreasure/buriedtreasure.phtml?%s,%s' % (x, y), 'http://www.neopets.com/pirates/buriedtreasure/buriedtreasure.phtml?')
+                    gamePrize = self.functions.getBetween(resp.text, '<b><center>', '</center></b>')
+                    self.functions.log('Buried Treasure: %s' % gamePrize)
+                    self.functions.updateLastRun('BuriedTreasure', username)
+            else:
+                self.functions.log('Buried Treasure: Your account is too young to play')
